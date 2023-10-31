670b8d8f68d8ca66c14ced4e1082af96482a314b
anaysah <ajaysahu0606@gmail.com>
2023-10-31 23:15:01 +0530

    fixed student menu links in sidemenu.inc.php
    the sidemenu was not working in the student dasboard
    the student sidemenu dont show the "+roll call" button but tried to add link to that element which is not created

--- templates/sidemenu.inc.php
+++ templates/sidemenu.inc.php
@@ -3,36 +3,40 @@
 
 <div class="side-menu box" id="side-menu">
     <?php
-    if ($_SESSION['userType']==="teacher"):?>
-    <div class="side-menu-top" id="side-menu-top">
-        <span class="border add-class flex-left">
-            <i class="fa-sharp fa-solid fa-plus fa-lg"></i><span>Roll Call</span>
-        </span>
-    </div>
+    if ($_SESSION['userType'] === "teacher"): ?>
+        <div class="side-menu-top" id="side-menu-top">
+            <span class="border add-class flex-left">
+                <i class="fa-sharp fa-solid fa-plus fa-lg"></i><span>Roll Call</span>
+            </span>
+        </div>
     <?php endif ?>
 
     <div class="side-menu-mid d-flex flex-column gap-1">
 
-        <span class="side-menu-btn" id="attendance"><i class="fa-solid fa-notes-medical fa-lg"></i><span>Attendance</span></span>
+        <span class="side-menu-btn" id="attendance"><i
+                class="fa-solid fa-notes-medical fa-lg"></i><span>Attendance</span></span>
         <span class="side-menu-btn" id="peoples"><i class="fa-solid fa-users fa-lg"></i><span>Peoples</span></span>
-            </div>
+    </div>
     <div class="side-menu-down d-flex flex-column gap-1">
-        <span class="side-menu-btn" id="classes_label"><i class="fa-solid fa-users-rectangle fa-lg"></i><span>Classses</span></span>
+        <span class="side-menu-btn" id="classes_label"><i
+                class="fa-solid fa-users-rectangle fa-lg"></i><span>Classses</span></span>
         <!-- <hr class="m-0"> -->
         <?php
         $classes = giveClasses($conn, $_SESSION['id'], $_SESSION['userType']);
         // $activeClass = $_COOKIE['class_id'];
-
+        
         if (count($classes) > 0) {
-            foreach ($classes as $class) {
-        ?>
-        <a href="" class="class-name-btn" id="class_<?= $class['class_id'] ?>" onclick="setClassIdCookie('<?= $class['class_id'] ?>','<?= $class['class_name'] ?>')">
-            <span>
-                <?= $class['class_name'] ?> <?= $class['section'] ?>
-            </span></a>
-        <!-- <hr class="m-0"> -->
-        <?php
-            }
+            foreach ($classes as $class):
+                ?>
+                <a href="" class="class-name-btn" id="class_<?= $class['class_id'] ?>"
+                    onclick="setClassIdCookie('<?= $class['class_id'] ?>','<?= $class['class_name'] ?>')">
+                    <span>
+                        <?= $class['class_name'] ?>
+                        <?= $class['section'] ?>
+                    </span></a>
+                <!-- <hr class="m-0"> -->
+                <?php
+            endforeach;
         } else {
             echo "No classes found.";
         }
@@ -51,7 +55,8 @@
     }
     for (let id in links) {
         const myDiv = document.getElementById(id);
-        myDiv.addEventListener("click", function() {
+        if(!myDiv) continue;
+        myDiv.addEventListener("click", function () {
             window.location.href = links[id];
         });
     }
@@ -63,11 +68,11 @@
     }
     //---change the cokkie end here
 
-    const activeClass =document.getElementById("class_<?= $_COOKIE['class_id']; ?>");
+    const activeClass = document.getElementById("class_<?= $_COOKIE['class_id']; ?>");
     activeClass.classList.add('active-class');
 
     const menuBarBtn = document.getElementById("side-menu-bar-btn");
-    menuBarBtn.addEventListener("click",()=>{
+    menuBarBtn.addEventListener("click", () => {
         const sideMenu = document.getElementById("side-menu");
         sideMenu.classList.toggle("show");
         console.log(sideMenu)
